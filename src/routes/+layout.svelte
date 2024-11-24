7fbe83631788ae03529b6c821b330b3b4ad0e3fe
matheus <matheussmoura@outlook.com>
2024-11-24 15:39:44 -0300

    fix: svelte config should be a js file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  let { children } = $props();
 </script>
 
 <div class="mx-4 sm:mx-0 my-4">
   <div class="container mx-auto">
-    <slot />
+    {@render children()}
   </div>
 </div>
